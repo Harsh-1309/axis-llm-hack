d3a8e78e6a8a6b4ca022c881a3229e695dcb3d2b
Harsh1309 <harsh13092001@gmail.com>
2023-08-15 03:34:14 +0530

    add more routes

--- app/application.py
+++ app/application.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import json
 from jd_score_better.jd_score_better import *
+from resume_ranker.extract_resume_data import *
+from resume_ranker.score_resume import *
+from cv_questions.cv_questions import *
 import openai
 
 app = Flask(__name__)
@@ -27,14 +30,14 @@ def save_data(data):
 def index():
     data = load_data()
     if request.method == "POST":
+
         job_title = request.form["job_title"]
         job_description = request.form["job_description"]
-        accept_desc=request.form["accept_desc"]
+        accept_option = request.form.get("accept_option")
         score = scoreJobDescription(job_title,job_description)['job_description_score']
         improvement = improveJobDescription(job_title,job_description)['improved_job_description']
-        
-        accepted_values=["yes","Yes","y","Y"]
-        if accept_desc in accepted_values:
+
+        if accept_option == "yes":
             accept_desc="yes"
         else:
             accept_desc="no"
@@ -51,8 +54,76 @@ def index():
         )
         save_data(data)
 
-        return render_template("index.html", job_title=job_title, job_description=job_description, score=score, improvement=improvement)
+        return render_template("index.html", job_title=job_title, job_description=job_description, score=score, improvement=improvement, accept_desc=accept_desc)
     return render_template("index.html")
 
+
+@app.route("/extract_resumes", methods=["GET", "POST"])
+def extract_resumes():
+    if request.method == "POST":
+        folder_path = request.form["folder_path"]
+        data_=extract_all_resume_data(folder_path)
+        
+        return "Resumes extracted successfully." #data_
+    return render_template("extract_resumes.html")
+
+@app.route("/score_resumes", methods=["GET", "POST"])
+def score_resumes():
+    if request.method == "POST":
+        score_option = request.form.get("score_option")
+
+        if score_option == "yes":
+            data_=score_all_resumes()
+            return "Resumes scored successfully."
+        else:
+            return "Resumes not scored."
+
+    return render_template("score_resumes.html")
+
+
+# @app.route("/shortlist_candidates", methods=["GET", "POST"])
+# def shortlist_candidates():
+#     if request.method == "POST":
+#         num_candidates = int(request.form["num_candidates"])
+#         return redirect(f"/generate_questions/{num_candidates}")
+
+#     return render_template("shortlist_candidates.html")
+
+# @app.route("/generate_questions/<int:num_candidates>", methods=["GET", "POST"])
+# def generate_questions(num_candidates):
+#     if request.method == "POST":
+#         generate_option = request.form.get("generate_option")
+
+#         if generate_option == "yes":
+#             data_=generate_questions_for_shortlisted(num_candidates)
+#             return "Questions generated successfully."
+#         else:
+#             return "Questions not generated."
+
+#     return render_template("generate_questions.html")
+
+@app.route("/shortlist_candidates", methods=["GET", "POST"])
+def shortlist_candidates():
+    if request.method == "POST":
+        num_candidates = int(request.form["num_candidates"])
+        return redirect(url_for("generate_questions", num_candidates=num_candidates))
+
+    return render_template("shortlist_candidates.html")
+    # return redirect(url_for("generate_questions", num_candidates=num_candidates))
+
+@app.route("/generate_questions/<int:num_candidates>", methods=["GET", "POST"])
+def generate_questions(num_candidates):
+    if request.method == "POST":
+        generate_option = request.form.get("generate_option")
+
+        if generate_option == "yes":
+            # Run your script to generate questions
+            # Replace "generate_questions_script.py" with the actual script filename
+            data_=generate_questions_for_shortlisted(num_candidates)
+            return "Questions generated successfully."
+        else:
+            return "Questions not generated."
+
+    return render_template("generate_questions.html", num_candidates=num_candidates)
 if __name__ == "__main__":
     app.run(debug=True)
